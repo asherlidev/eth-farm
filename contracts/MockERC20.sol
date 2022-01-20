24fc42d1537164168fb655a9abfb2c40931f5006
asherlidev <asherlidev95@gmail.com>
2022-01-20 20:46:38 +0000

    create ERC20 dai contract for MockDai

---
+++ contracts/MockERC20.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MockERC20 is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Real", "RLT") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
