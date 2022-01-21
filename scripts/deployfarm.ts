c78e9abe952302e4bf231262c0691c4a766d8df7
asherlidev <asherlidev95@gmail.com>
2022-01-21 22:43:55 +0000

    add the deploy script

--- scripts/deployfarm.ts
+++ scripts/deployfarm.ts
@@ -5,6 +5,8 @@
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
+const kDAI = "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa";
+
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
@@ -14,12 +16,23 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const [deployer] = await ethers.getSigners();
 
-  await greeter.deployed();
+  const MockDai = await ethers.getContractFactory('MockERC20');
+  const mockDai = await MockDai.deploy(1000000);
+  console.log('mockDai address: ' + mockDai.address);
 
-  console.log("Greeter deployed to:", greeter.address);
+  const RealToken = await ethers.getContractFactory("RealToken");
+  const realToken = await RealToken.deploy(1000000);
+  console.log('realToken address: ' + realToken.address);
+
+  const RealFarm = await ethers.getContractFactory("RealFarm");
+  const realFarm = await RealFarm.deploy(kDAI, realToken.address);
+  console.log('realFarm address: ' + realFarm.address);
+
+  await realToken.transferOwnership(realFarm.address);
+  console.log('realToken ownership transferred to :' + realFarm.address);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
